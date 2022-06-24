d995824d433f684a8ec8e01283e2d53950c291b5
Geoffrey <geoffreynjihia15@gmail.com>
2022-06-24 20:42:36 +0300

    adding event listener to the item description

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '68a65b4a39mshcf2679edfb6b35ap1eda53jsnfed9262c1f89',
-		'X-RapidAPI-Host': 'the-giving-lab-thegivinglab.p.rapidapi.com'
-	}
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-fetch('https://the-giving-lab-thegivinglab.p.rapidapi.com/users/user/%7BID%7D/activity/?apikey=undefined', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+    const form = document.getElementById("description-form")
+    form.addEventListener('submit', function (event) {
+        event.preventDefault()
 
-const form = document.getElementById("description-form")
+        const fullName = document.getElementById('fname').value
+        console.log(fullName)
 
-form.addEventListener('submit', function (event){
-    event.preventDefault()
+        const yourEmail = document.getElementById('email').value
+        console.log(yourEmail)
+
+        const itemDescription = document.getElementById('description-form1').value
+        console.log(itemDescription)
+    })
 })
 
-const contactForm = document.getElementById('contact-form')
-form.addEventListener('submit', function (event){
-    event.preventDefault()
-})
